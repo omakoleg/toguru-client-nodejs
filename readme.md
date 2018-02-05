0e71093bdc22333ae83db16ab8eecf99a0d96d8d
bdadam <me@bdadam.com>
2018-02-05 01:46:54 +0100

    implemented basic usage without express

--- readme.md
+++ readme.md
@@ -1,5 +1,29 @@
 # Toguru Client for Node.js
 
+## Usage with plain Node
+```js
+const ToguruClient = require('toguru-client');
+const client = ToguruClient({
+    endpoint: 'https://example.com/togglestate',
+    refreshInterval: 60 * 1000 // 1 minute
+});
+
+client.ready().then(() => {
+    client.isToggleEnabled('t1', 'ad89f957-d646-46e9-b6da-a02c9aa7faba');
+    // true or false
+    
+    client.toggles();
+    // [{ id: 't1', rolloutPercentage: 50, }, { id: 't2', rolloutPercentage: 70 }, { id: 't3', tags: { services: 'svc1,svc4' } }]
+    
+    client.togglesForService('service');
+    // [{ id: 't3', tags: { services: 'service,svc4' } }]
+    
+    client.toggleNamesForService('service');
+    // ['t3']
+});
+
+```
+
 ## Usage with express
 ```js
 const toguruClient = require('toguru-client/express');
---
+++ src/client.js
@@ -0,0 +1,37 @@
+const calculateBucket = require('./calculate-bucket');
+const toggleHandler = require('./toggle-handler');
+
+const getJson = require('bent')('json');
+
+
+module.exports = ({ endpoint, refreshInterval = 60000 }) => {
+    let toggles = [];
+
+    const reload = async () => {
+        toggles = await getJson(endpoint);
+    };
+
+    const initialize = async () => {
+        setInterval(reload, refreshInterval);
+        await reload();
+    };
+
+    const ready = initialize();
+
+    return {
+        ready: () => ready,
+        isToggleEnabled: (toggleName, uuid) => {
+            const bucket = calculateBucket(uuid);
+            return toggleHandler.isToggleEnabled(toggleName, toggles, bucket);
+        },
+        toggles: () => {
+            return toggles;
+        },
+        togglesForService: (service) => {
+            return toggleHandler.togglesForService(service, toggles);
+        },
+        toggleNamesForService: service => {
+            return toggleHandler.togglesForService(service, toggles).map(t => t.id);
+        }
+    };
+};
