ebe9fc11ab78e7f5fb3d2d63213b625a4abc9451
Yifan Yu <yifan.9916@gmail.com>
2019-07-17 13:20:06 +0200

    fix fallback bucket count value

--- src/services/calculate-bucket.js
+++ src/services/calculate-bucket.js
@@ -1,13 +1,23 @@
 const BN = require('bn.js');
 
+// This calculation is broken.
+// Normally we would calculate with new BN(x).mod(new BN(100)).toNumber() + 1;
+// BUT: the existing scala client is doing strange calculation.
+// See thread here: https://github.com/Scout24/toguru-scala-client/pull/27
 module.exports = (uuid) => {
-    // This calculation is broken.
-    // Normally we would calculate with new BN(x).mod(new BN(100)).toNumber() + 1;
-    // BUT: the existing scala client is doing strange calculation.
-    // See thread here: https://github.com/Scout24/toguru-scala-client/pull/27
+    // in case of errors with the uuid we need a fallback value
+    // we set it to 100 so that the feature is still rolled out to all users when the rollout percentage is at 100
+    const FALLBACK_BUCKET_COUNT = 100;
+
+    if (!uuid) {
+        return FALLBACK_BUCKET_COUNT;
+    }
+
     const strippedUUID = uuid.replace(/-/g, '');
-    if (strippedUUID.length !== 32) {
-        return 1;
+    const isUUIDInvalid = strippedUUID.length !== 32;
+
+    if (isUUIDInvalid) {
+        return FALLBACK_BUCKET_COUNT;
     }
 
     const hi = new BN(strippedUUID.substr(0, 16), 16);
@@ -24,5 +34,6 @@ module.exports = (uuid) => {
     if (bucketCount < 0) {
         return 100 + bucketCount;
     }
+
     return bucketCount;
 };
